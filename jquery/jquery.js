dc1569b0a4b1345e725cbf033a1eda3f8d4f8213
agustina hardy <agustinahardy@live.com.ar>
2021-08-30 19:09:24 -0300

    first commit

---
+++ jquery/jquery.js
@@ -0,0 +1,46 @@
+/* ESTE ES EL FORMULARIO QUE VA
+                    <form id="template-form">
+                    <div class="mb-3">
+                    <label class="form-label">Email</label>
+                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-3">
+                    <label class="form-label">Numeric Password</label>
+                    <input type="numbers" class="form-control" id="274012">
+                    </div>
+                    <div class="mb-3 form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Enviar notificaciones</label>
+                    </div>
+                    <button type="submit" class="btn btn-dark">Iniciar cuenta</button>
+                    </form>
+*/
+
+//Formulario - Crear cuenta
+$('form').prepend(` <form id="template-form">
+                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <input type="numbers" class="form-control" id="274012">
+                    <div class="mb-3 form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Enviar notificaciones</label>
+                    </div>
+                    <button type="submit" class="btn btn-dark">Iniciar cuenta</button>
+                    </form>
+                `);
+
+$("#template-form").submit(function (e) {
+    e.preventDefault();
+    let datos = $ (e.target).children()
+    //email
+    console.log(datos[0].value);
+    //password
+    console.log(datos[1].value);
+});
+
+
+//Efecto con jQuery
+$('h6').prepend('<h6 class="avisoCompra">Aguardá unos segundos..</h6>');
+
+$('.avisoCompra').css('color', 'rgb(121, 117, 113)')
+.slideUp(2000)
+.delay(2000)
---
+++ js/domSearch.js
@@ -0,0 +1,5 @@
+import { searchCards } from "./searchCards.js";
+
+document.addEventListener("DOMContentLoaded", (e) => {
+    searchCards(".search", ".cards");
+});
